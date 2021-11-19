c75231fab2a4484955e9663d1c2e1fa56c111277
montionH <66965471+montionH@users.noreply.github.com>
2021-11-18 21:07:31 -0700

    Delete p3 directory

--- p3/modelo_no_bloqueante.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-console.log('\nabriendo archivo...')
-
-var content = fs.readFileSync('el_quijote.txt', 'utf8', function(error, content) {
-    console.log(content)
-})
-console.log(content)
-console.log('\nhaciendo otra cosa')
-console.log(process.uptime())
